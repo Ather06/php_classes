26bca90db18877c1e59cb6e1560777c38fca9ea0
Ather06 <mdatherbiswas06@gmail.com>
2023-10-29 09:15:01 +0600

    php class start

---
+++ Day_1/Variable.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = 'Bangladesh'; //string variable
+$age = 55; //number variable
+$question = 'How are you';
+
+echo $name; // output dekhate echo use 
+echo "\n"; // php te new line korar jonno \n use kora hoy
+echo $age;
+
+echo "\n";
+// variable diye full sentence 
+
+echo "hellow {$name},{$question}\n";
+echo "hellow {$name},{$question}\n";
