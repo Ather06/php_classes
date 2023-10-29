84c03df8766cd99e8503cae43884b42354f7c51a
Ather06 <mdatherbiswas06@gmail.com>
2023-10-29 10:35:09 +0600

    variable & comments added

---
+++ Day_1/variable2_comments.php
@@ -0,0 +1,18 @@
+<?php
+
+$task = 'read';
+echo $task;
+echo "\n";
+$task = 'write';
+echo $task;
+echo "\n";
+
+// php variable is immuteable.It can change
+
+/* 
+constant variable 
+*/
+
+define ('PI', 3.1416);
+
+echo "Value of PI = ".PI;
