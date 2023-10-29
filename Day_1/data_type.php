ed6cdaae5e055af4baecd26becf0fd89c2f70759
Ather06 <mdatherbiswas06@gmail.com>
2023-10-29 12:35:53 +0600

    finalize

--- Day_1/data_type.php
+++ Day_1/data_type.php
@@ -31,3 +31,28 @@ echo "\n";
 // float data type
 $number = 3.1416;
 var_dump ($number);
+
+//Printf er kaj
+$fname = 'Ather';
+$lname = 'Biswas';
+printf("His %s name is %s %s ", "Full", $fname,$lname);
+echo "\n";
+/*
+seiral 
+%s = Full
+%s = $fname
+%s = $lname
+
+*/
+// 3 ways to write same segment sentence
+$day1 = 'Saturday';
+$day2 = 'Sunday';
+
+// first way is to use dot sign and direct variable name which is wrap without double inverted commma
+echo "First day is " .$day1." and second day is ".$day2."\n";
+
+// second way is to use second bracket sign and variable name which is wrap double inverted comma
+echo "First day is {$day1} and second day is {$day2} \n";
+
+//third way is to use %s which is wrap double inverted commma and %s serial means 1,2,3 variable
+printf("First day is %s and second day is %s", $day1,$day2);
