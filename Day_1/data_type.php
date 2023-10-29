69aea2e596a9abb4d5da5f4a300281f12879839d
Ather06 <mdatherbiswas06@gmail.com>
2023-10-29 11:04:36 +0600

    data type added

---
+++ Day_1/data_type.php
@@ -0,0 +1,30 @@
+<?php
+/*
+different types of data type..
+
+1. Integer (int)
+2.Double/ float
+3.string
+4.Boolean (bool)
+5.Null
+6.Object
+7.Resourse
+
+*/
+// data type information janar jonno var_dump use kora hoi
+
+// string data type
+$name = 'Bangladesh';
+var_dump ($name);
+echo "\n";
+// integer data type
+$age = 12;
+var_dump ($age);
+echo "\n";
+// boolean data type
+$result = true;
+var_dump ($result);
+echo "\n";
+// float data type
+$number = 3.1416;
+var_dump ($number);
