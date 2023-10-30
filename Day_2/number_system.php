862d81ed07386c55016964ba7fe35f1e74f386f3
Ather06 <mdatherbiswas06@gmail.com>
2023-10-30 23:04:11 +0600

    number type added

---
+++ Day_2/number_system.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+different types of number system
+1.Decimal
+2.Octal
+3.Hexadecimal
+4.Binary
+*/
+// $d = 12;
+// $o = 015;
+// $h = 0x3D;
+// printf ("The number is %d and %d and %d \n",$d,$o,$h,);
+// printf("The binary equvalent of %d is %b \n", 252,252);
+// printf("The octal equvalent of %d is %o \n", 252,252);
+// printf("The hexadecimal equvalent of %d is %X \n", 2521,2521);
+
+
+//practise purpose
+
+$a = 23;
+$b = 016;
+$m = 0x22A;
+printf("The number is %d and %d and %d \n", $a, $b, $m);
+printf("The binary equvalent of %d is %b \n",20,20);
+printf("The Octal equvalent of %d is %o \n",20,20);
+printf("The Octal equvalent of %d is %X \n",200,200);
